8561c21ebd958dc340785a738ffce52792e45c83
daj0606 <127715000+daj0606@users.noreply.github.com>
2024-02-15 22:23:29 -0600

    Initial Commit

---
+++ src/main/java/contoller/AddToTotal.java
@@ -0,0 +1,49 @@
+/**
+ * @author ${user} - djackson16
+ * CIS175 - Spring 2024
+ * 2/15/2024
+ */
+package contoller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.GetTotal;
+
+/**
+ * Servlet implementation class AddToTotal
+ */
+@WebServlet("/addToTotal")
+public class AddToTotal extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddToTotal() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userText1 = request.getParameter("additionNum1");
+		String userText2 = request.getParameter("additionNum2");
+		GetTotal total = new GetTotal(userText1, userText2);
+		double outText = total.Addition();
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("Added Numbers: " + outText);
+		writer.close();
+		
+	}
+
+}
